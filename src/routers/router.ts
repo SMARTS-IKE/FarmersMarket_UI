acccaf4f9a7108f1ccdb278eb8cc85fa5a429baf
Test <test@example.com>
2026-05-31 15:49:35 +0300

    Fixed the selected tab color

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,8 +1,11 @@
 import { createRouter } from '@tanstack/react-router';
+import { createHashHistory } from '@tanstack/history';
 import { adminRouteTree } from './adminRoutes';
 import { authRouteTree } from './authRoutes';
 import { userRouteTree } from './userRoutes';
 import { indexRoute, rootRoute } from './baseRoutes';
+
+const history = createHashHistory();
 
 const routeTree = rootRoute.addChildren([
   indexRoute,
@@ -11,7 +14,7 @@ const routeTree = rootRoute.addChildren([
   userRouteTree,
 ]);
 
-export const router = createRouter({ routeTree });
+export const router = createRouter({ routeTree, history });
 
 declare module '@tanstack/react-router' {
   interface Register {
